817ac9fda7478096c3e670ae04a7c0d9c2b98126
tomasjuliano <tjuliano@frba.utn.edu.ar>
2024-08-28 19:07:52 +0000

    Terminando la etapa 2, con error en leer_consola y sin hacer la seccion de strings.

--- client/src/client.c
+++ client/src/client.c
@@ -19,20 +19,29 @@ int main(void)
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
 
+	//log_info(logger, "Soy un log"); //funcion para escribir en el log (una vez, ya que queda guardado en tp0.log)
+
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
 	config = iniciar_config();
+	valor = config_get_string_value(config, "CLAVE");
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
 	// Loggeamos el valor de config
 
+	log_info(logger, "%s", valor); //funcion para escribir en el log (una vez, ya que queda guardado en tp0.log)
+	log_info(logger, "%s", ip);  //funcion para escribir en el log (una vez, ya que queda guardado en tp0.log)
+	log_info(logger, "%s", puerto); //funcion para escribir en el log (una vez, ya que queda guardado en tp0.log)
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
 	leer_consola(logger);
+
 
 	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
 
@@ -54,14 +63,28 @@ int main(void)
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create("tp0.log", "Tomas", 1, LOG_LEVEL_INFO);
+
+	if (nuevo_logger == NULL) {
+		// ¡No se pudo crear el config!
+		// Terminemos el programa
+		log_error(nuevo_logger, "No se pudo crear el log.");
+		abort();
+	}
 
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config;
+	t_config* nuevo_config = config_create("cliente.config");
+
+	if (nuevo_config == NULL) {
+		// ¡No se pudo crear el config!
+		// Terminemos el programa
+		log_error(nuevo_config, "No se pudo crear el config.");
+		abort();
+	}
 
 	return nuevo_config;
 }
@@ -71,12 +94,23 @@ void leer_consola(t_log* logger)
 	char* leido;
 
 	// La primera te la dejo de yapa
-	leido = readline("> ");
+	//leido = readline("> ");
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
+    while (1) {
+        leido = readline(">");
 
+        if (leido == NULL) {
+            break;
+        }
+
+		log_info(logger, leido);
+        printf("%s\n", leido);
+        free(leido);
+    }
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
+	//free(leido);
 
 }
 
@@ -97,4 +131,6 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	  log_destroy(logger);
+	  config_destroy(config);
 }
